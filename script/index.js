2cc46d05edc88755ae7fa05bde39754542fbaa40
joao vitor <githubconta2021@gmail.com>
2023-04-03 16:23:45 -0400

    nova funcionalidade feita

--- script/index.js
+++ script/index.js
@@ -1,5 +1,18 @@
+//  soma
+
 function soma(numero01_, numero02_) {
     return numero01_ + numero02_; 
 }
 
-soma();
+soma(value, value01);
+
+// subtrair
+
+function subtrair(numero01_, numero02_) {
+    return numero01_ - numero02_; 
+}
+
+subtrair(value, value02); 
+
+
+
