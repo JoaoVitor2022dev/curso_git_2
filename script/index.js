8dfdd2737a4af183ff45ea33b7773c307e8d1e3a
joao vitor <githubconta2021@gmail.com>
2023-04-07 15:57:31 -0400

    finalizei a class de cadastro do site

--- script/index.js
+++ script/index.js
@@ -16,3 +16,20 @@ subtrair(value, value02);
 
 
 
+// classe de cadastro de dados...
+
+class Apresentaçao {
+    constructor(name, idade , estadoCivil){
+        this.name = name; 
+        this.idada = idade; 
+        this.estadoCivil = estadoCivil;  
+    }
+
+    static meusDados() {
+        console.log(`Meu nome é ${this.name} e eu tenho ${this.idada} de idade, e no momento estou ${this.estadoCivil}`);
+    }
+}
+
+const joao_vitor = new Apresentaçao('vitor lima', 22, 'compromedito');
+
+Apresentaçao.meusDados(joao_vitor);
